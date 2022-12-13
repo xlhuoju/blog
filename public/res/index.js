c55dd5c9cdb8a5953d3bacded89513fc96007038
Ar-Sr-Na <arsrna.cn@outlook.com>
2022-12-13 09:30:31 +0800

    文章描述

--- public/res/index.js
+++
@@ -1,7 +0,0 @@
-const aegis = new Aegis({
-    id: 'PR8Q4VBmZvWyA3gmKO', // 上报 id
-    uin: 'xxx', // 用户唯一 ID（可选）
-    reportApiSpeed: true, // 接口测速
-    reportAssetSpeed: true, // 静态资源测速
-    spa: true, // spa 应用页面跳转的时候开启 pv 计算
-  });
